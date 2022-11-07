2cc683dff36bc341c7bf29f0309254c182a112f7
Edwin Huang <edwin.huang7@gmail.com>
2022-11-07 21:09:41 +1100

    improvements

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,15 +3,32 @@ import "./About.scss";
 import { images } from "../../constants";
 
 const info = [
-  { title: "Safe Documents", description: "LawConnect stores its data on servers provided by Amazon Web Services and is safer than email.", imgUrl: images.safeDocs },
-  { title: "Work Remotely", description: "You can sign in to LawConnect and view,collaborate and share your documents from anywhere..", imgUrl: images.workRemote },
-  { title: "Greater Collaboration", description: "Collaborate on documents with clients, lawyers and other parties with LawConnect's Comment and Reply.", imgUrl: images.greaterCollab },
+  {
+    title: "Safe Documents",
+    description:
+      "LawConnect stores its data on servers provided by Amazon Web Services and is safer than email.",
+    imgUrl: images.safeDocs,
+  },
+  {
+    title: "Work Remotely",
+    description:
+      "You can sign in to LawConnect and view,collaborate and share your documents from anywhere.",
+    imgUrl: images.workRemote,
+  },
+  {
+    title: "Greater Collaboration",
+    description:
+      "Collaborate on documents with clients, lawyers and other parties with LawConnect's Comment and Reply.",
+    imgUrl: images.greaterCollab,
+  },
 ];
 const About = () => {
   return (
     <>
       <div className="app__about">
-        <h2 className="large-text-centered">One Solution Designed to Make Your Firm More Money</h2>
+        <h2 className="large-text-centered">
+          One Solution Designed to Make Your Firm More Money
+        </h2>
         <div className="app__profiles">
           {info.map((about) => (
             <div className="app__profile-item">
